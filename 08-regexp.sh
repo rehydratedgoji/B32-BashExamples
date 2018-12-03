d906a19d404418f6502b9b03a2d42e2e0370f2a2
Ariela Somfelean <arielasom@gmail.com>
2018-12-03 11:11:13 -0500

    done with regular expressions

--- 08-regexp.sh
+++ 08-regexp.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 
+echo -n "Texte : "
+
+read texte
+
+if [[ $texte =~ ^[0-9]+$  ]];then #commence par ^ et finit toute de suite après
+# + = au moins au fois et répéter à l'infini
+# ~ = ce qui suit est une xpression reguliere at  non pas que ca doit etre egal à ça
+# ^ = commence par 
+# []= entre 
+# * =  pas utilisé ici mais similaire au + sauf que ca accepterait aucune valuer  aussi
+	echo "Numérique"
+fi

8b6e5121377dd94c9a954f33428350b027d9d4b1
Ariela Somfelean <arielasom@gmail.com>
2018-12-03 10:09:35 -0500

    done with calculations

--- 03-calculs.sh
+++ 03-calculs.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 
+echo 10+19
+#to really execute and get teh result you have to use the $() and the ()
+echo $((10+19))
+
+read -p "Nombre 1: " premier
+#remember no $ when we want to assign values to variables. 
+read -p "Nombre 2:" second
+
+let resultat=$premier*$second
+#another way to write it so you don't have to add the parenthesis
+echo $resultat

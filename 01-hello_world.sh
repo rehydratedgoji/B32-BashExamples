f84b87454b8900cad583483a9412300a14d89e99
Ariela Somfelean <arielasom@gmail.com>
2018-12-03 09:50:06 -0500

    done with 01-hello_world

--- 01-hello_world.sh
+++ 01-hello_world.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
 
+# Ceci est un commentaire
+echo "Salut"
+
+echo -n "vive "
+# -n = empêche retour de  ligne
+echo "Linux"
+
+echo -e  "123\n123\n123"
+# -e = interprets special characters
+
+read -p "Vous avex quel âge?" age
+# read = reads variables?
+
+#when we assign a variable, we don't need to add a $ only when we call it
+echo $age "ans! Vous paraissez plus jeune"

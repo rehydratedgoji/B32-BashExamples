65dfa79e88eea64792b3311c4e2cd5d1671bf8d0
Ariela Somfelean <arielasom@gmail.com>
2018-12-03 10:02:19 -0500

    Done with variables

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
 
+prenom=KimCharles
+#naem sure you don't add unnecessary spaces
+echo $prenom
+#force a declaration of a variable "int"
+declare -i age=23
+
+#make a variable READ ONLY-const in c++
+declare -r age2=33
+
+# system variables
+echo $0
+#$0 = the first srgument or always the name of the file to be executed
+
+echo $1
+#first argument used when calling the script, 1-9
+echo $2
+
+echo $#
+#tells you how many arguments we have , like argc in C

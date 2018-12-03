64cfbbdc054bcd0fc661793738d2619409c189ee
Ariela Somfelean <arielasom@gmail.com>
2018-12-03 12:21:18 -0500

    exercice2 et 1

--- exercice1.sh
+++ exercice1.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
 # La ligne suivante permet de vérifier si un mot existe dans le dictionnaire
-# Il faut suffixer cet URL avec le mot entré par l'uasger (ex: word=chat)
+read word
+
+resultat=`wget -qO - http://dictionary.objectif8.com/exists.php?word=$word`
+if test $resultat = "1"
+then 
+	echo "Le mot existe!"
+else
+
+	echo "Le not n'exite pas..."
+fi
+
+
 #wget -qO - http://dictionary.objectif8.com/exists.php?word=
--- exercice2.sh
+++ exercice2.sh
@@ -1,1 +1,40 @@
 #!/bin/bash
+
+
+menu() {
+	echo "====================="
+	echo "     CVM Soft        "
+	echo "    d=dictionnnaries "
+	echo "    q=quitter        "
+	echo "====================="
+}
+menu 
+
+#while [[ $choix != "q ]]
+#do
+#	menu
+#	read -p "Choix : " choix
+#
+#	if [[ $choix = "D" ]];then
+#			./exercice1.sh
+#			sleep 1
+#	fi
+#done #prof
+
+#clear
+
+read Reponse
+if test  $Reponse = "d"
+	then
+	./exercice1.sh
+
+elif test $Reponse != "q"
+	then
+	echo "Reponse non valide"
+	./exercice2.sh
+elif test $Reponse = "q"
+	then
+	echo "bye!"
+	
+fi
+

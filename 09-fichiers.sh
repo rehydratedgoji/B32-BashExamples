cd236f1bd2f0983cf09e28c73c2fa44cb1d0749c
Ariela Somfelean <arielasom@gmail.com>
2018-12-03 11:16:52 -0500

    finished with files and directories

--- 09-fichiers.sh
+++ 09-fichiers.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+
+if test -e $1 # -e = does it exist?
+
+then 
+	echo "existe"
+
+	if test -d $1 # -d= is it a  directory
+	then 
+		echo "Il est un répertoire"
+	fi
+else
+	echo "inexistant"
+fi
+
